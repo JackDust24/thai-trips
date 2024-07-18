eac98d6be122cc3c4c4facdfbf11b5a484c822da
Jason Whittaker <jason.whittaker@7peakssoftware.com>
2024-07-18 20:23:36 +0700

    hotfix: remove the redirect unfinished 2

--- src/app/(overview)/orders/page.tsx
+++ src/app/(overview)/orders/page.tsx
@@ -5,13 +5,14 @@ import { redirect } from 'next/navigation';
 
 //TODO: Set up page
 export default function MyOrdersPage() {
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      // redirect('/api/auth/signin?callbackUrl=/orders');
-      redirect('/');
-    },
-  });
+  //TODO: WHen properly set up implement this:
+  // const { data: session } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     redirect('/api/auth/signin?callbackUrl=orders');
+  //     // redirect('/');
+  //   },
+  // });
 
   return <div>Page Under Construction</div>;
 }
